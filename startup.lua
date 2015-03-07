f48bb2f0d80170b2db41736c396d3fc90e781c60
Jan Jansen <farodin91@googlemail.com>
2015-03-07 14:32:44 +0100

    let work the startup skript

--- startup.lua
+++ startup.lua
@@ -5,13 +5,15 @@ local branch = "master"
 local repo = "GateControlCC"
 
 local function getFileLink(path)
-	return "https://raw.githubusercontent.com/"..user.."/"..repo.."/"..branch.."/"..path
+  return "https://raw.githubusercontent.com/"..user.."/"..repo.."/"..branch.."/"..path
 end
 
 for i,v in ipairs(libs) do
+  print("Download..."..getFileLink(v[2]))
   local f = http.get(getFileLink(v[2])) 
   local h = fs.open(v[1], "w")
   h.write(f.readAll())
+  h.close()
   --shell.run("pastebin","get "..v[2].." "..v[1])
 end
 
